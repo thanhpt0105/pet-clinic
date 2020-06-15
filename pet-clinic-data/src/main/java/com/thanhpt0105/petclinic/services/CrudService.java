8eea8210bcc2d7d6e2d5e4a8a6e02f34ebfcf533
thanhpt0105 <thanhpt0105@gmail.com>
2020-06-15 10:45:40 +0800

    Refactor to common CRUD interface. Close #10.

---
+++ pet-clinic-data/src/main/java/com/thanhpt0105/petclinic/services/CrudService.java
@@ -0,0 +1,16 @@
+package com.thanhpt0105.petclinic.services;
+
+import java.util.Set;
+
+public interface CrudService<T, ID> {
+
+    Set<T> findAll();
+
+    T findById(ID id);
+
+    T save(T object);
+
+    void delete(T object);
+
+    void deleteById(Long id);
+}
--- pet-clinic-data/src/main/java/com/thanhpt0105/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/thanhpt0105/petclinic/services/PetService.java
@@ -2,13 +2,8 @@ package com.thanhpt0105.petclinic.services;
 
 import com.thanhpt0105.petclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
+public interface PetService extends CrudService<Pet, Long> {
 
     Pet findById(Long id);
 
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
