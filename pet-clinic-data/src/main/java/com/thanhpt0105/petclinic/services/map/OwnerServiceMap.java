bf4d2efff446d40de648779e3e713379886aa162
thanhpt0105 <thanhpt0105@gmail.com>
2020-06-15 11:07:35 +0800

    Implement Map Base Service. Close #5.

---
+++ pet-clinic-data/src/main/java/com/thanhpt0105/petclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,38 @@
+package com.thanhpt0105.petclinic.services.map;
+
+import com.thanhpt0105.petclinic.model.Owner;
+import com.thanhpt0105.petclinic.services.OwnerService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+    @Override
+    public Owner findByLastName(String lastName) {
+        return null;
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/thanhpt0105/petclinic/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package com.thanhpt0105.petclinic.services.map;
+
+import com.thanhpt0105.petclinic.model.Pet;
+import com.thanhpt0105.petclinic.services.PetService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet,Long> implements PetService {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
