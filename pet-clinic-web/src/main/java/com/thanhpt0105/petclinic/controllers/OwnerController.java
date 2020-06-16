5304cc813a3c1aedf3b4d1f5e800ad722ef68783
thanhpt0105 <thanhpt0105@gmail.com>
2020-06-16 17:35:49 +0800

    Display list owner and vet.

--- pet-clinic-web/src/main/java/com/thanhpt0105/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/thanhpt0105/petclinic/controllers/OwnerController.java
@@ -1,13 +1,25 @@
 package com.thanhpt0105.petclinic.controllers;
 
+import com.thanhpt0105.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/", ""})
-    public String listVets() {
+    public String listOwners(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/ownerList";
     }
 }
