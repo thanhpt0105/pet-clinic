1e9c0bef1824175155f504f0b2091679fc0d880e
thanhpt0105 <thanhpt0105@gmail.com>
2020-06-18 15:12:30 +0800

    Add static resources.

--- pet-clinic-web/src/main/java/com/thanhpt0105/petclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/com/thanhpt0105/petclinic/controllers/IndexController.java
@@ -1,13 +1,15 @@
 package com.thanhpt0105.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
     @RequestMapping({"", "/", "index", "index.html"})
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("exampleText", "Example Text test reload");
         return "index";
     }
 }
